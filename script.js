7a689e10df6cf9ed75ae93a359ab762752f829cc
Rebecca Brietzke <rnbrietzke@outlook.com>
2021-02-15 16:54:18 -0600

    Added JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
-const ulTodo = document.getElementById('todo');
+const taskUl = document.getElementById('task');
+
+// Add Task Function
+const addTask = function (task) {
+	let taskText = input.value;
+
+	//Creating li
+	if (taskText) {
+		const taskLi = document.createElement('li');
+		const deleteBtn = document.createElement('button');
+		deleteBtn.classList.add('btn');
+
+		taskLi.appendChild(deleteBtn);
+
+		taskLi.innerText = taskText;
+
+		taskUl.appendChild(taskLi);
+
+		input.value = '';
+
+		taskLi.addEventListener('click', function () {
+			taskLi.classList.toggle('completed');
+		});
+	}
+};
+
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+
+	addTask();
+});
