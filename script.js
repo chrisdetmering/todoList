4f874fdb29f4133a0ee0ad153e5a816112dddb31
Rebecca Brietzke <rnbrietzke@outlook.com>
2021-02-18 11:12:10 -0600

    finished jsand modified css

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ const input = document.getElementById('input');
 const taskUl = document.getElementById('task');
 
 // Add Task Function
+
 const addTask = function (task) {
 	let taskText = input.value;
-
 	//Creating li
 	if (taskText) {
 		const taskLi = document.createElement('li');
 		const deleteBtn = document.createElement('button');
-		deleteBtn.classList.add('btn');
-
-		taskLi.appendChild(deleteBtn);
-
-		taskLi.innerText = taskText;
-
 		taskUl.appendChild(taskLi);
-
-		input.value = '';
-
-		taskLi.addEventListener('click', function () {
-			taskLi.classList.toggle('completed');
+		taskLi.textContent = taskText;
+		taskLi.setAttribute('id', 'li-toggle');
+		taskLi.addEventListener('dblclick', function (e) {
+			taskLi.remove();
 		});
+		input.value = '';
 	}
 };
+
+taskUl.addEventListener('click', toggleTodo);
+
+function toggleTodo(e) {
+	e.target.classList.toggle('completed');
+}
 
 form.addEventListener('submit', function (e) {
 	e.preventDefault();
